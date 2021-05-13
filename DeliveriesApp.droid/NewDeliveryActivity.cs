6746c365c2172886be03cf140fd04a370eb039a8
graham chukwumaobi <cheloghm@gmail.com>
2021-05-13 02:54:50 +0100

    implemented maps on android project

--- DeliveriesApp.droid/NewDeliveryActivity.cs
+++ DeliveriesApp.droid/NewDeliveryActivity.cs
@@ -1,5 +1,8 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Locations;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -13,10 +16,47 @@ using System.Text;
 namespace DeliveriesApp.droid
 {
     [Activity(Label = "NewDeliveryActivity")]
-    public class NewDeliveryActivity : Activity
+    public class NewDeliveryActivity : Activity, IOnMapReadyCallback, ILocationListener
     {
         Button saveButton;
         EditText packageNameEditText;
+        MapFragment mapFragment, destinationMapFragment;
+        double latitude, longitude;
+        LocationManager locationManager;
+
+        public void OnLocationChanged(Location location)
+        {
+            latitude = location.Latitude;
+            longitude = location.Longitude;
+            mapFragment.GetMapAsync(this);
+            destinationMapFragment.GetMapAsync(this);
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            MarkerOptions marker = new MarkerOptions();
+            marker.SetPosition(new LatLng(latitude, longitude));
+            marker.SetTitle("Your location");
+            googleMap.AddMarker(marker);
+
+            googleMap.MoveCamera(CameraUpdateFactory.NewLatLngZoom(new LatLng(latitude, longitude), 10));
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+            
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -26,15 +66,48 @@ namespace DeliveriesApp.droid
 
             saveButton = FindViewById<Button>(Resource.Id.saveButton);
             packageNameEditText = FindViewById<EditText>(Resource.Id.packageNameEditText);
+            mapFragment = FragmentManager.FindFragmentById<MapFragment>(Resource.Id.mapFragment);
+            destinationMapFragment = FragmentManager.FindFragmentById<MapFragment>(Resource.Id.destinationMapFragment);
 
             saveButton.Click += SaveButton_Click;
         }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            locationManager = GetSystemService(Context.LocationService) as LocationManager;
+            string provider = LocationManager.GpsProvider;
+
+            if (locationManager.IsProviderEnabled(provider))
+            {
+                locationManager.RequestLocationUpdates(provider, 5000, 1, this);
+            }
+
+            var location = locationManager.GetLastKnownLocation(LocationManager.NetworkProvider);
+            latitude = location.Latitude;
+            longitude = location.Longitude;
+            mapFragment.GetMapAsync(this);
+            destinationMapFragment.GetMapAsync(this);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            locationManager.RemoveUpdates(this);
+        }
         private async void SaveButton_Click(object sender, EventArgs e)
         {
+            var originLocation = mapFragment.Map.CameraPosition.Target;
+            var destinationLocation = destinationMapFragment.Map.CameraPosition.Target;
             Delivery delivery = new Delivery()
             {
                 Name = packageNameEditText.Text,
-                Status = 0
+                Status = 0,
+                OriginLatitude = originLocation.Latitude,
+                OriginLongitude = originLocation.Longitude,
+                DestinationLatitude = destinationLocation.Latitude,
+                DestinationLongitude = destinationLocation.Longitude
             };
 
             await Delivery.InsertDelivery(delivery);
